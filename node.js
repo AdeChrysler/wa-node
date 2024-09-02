fcfd80824fc72c562709a4332a24135033631e74
AdeChrysler <adechrysler@gmail.com>
2024-09-02 16:44:08 +0700

    Update node.js

--- node.js
+++ node.js
@@ -2,6 +2,7 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const qrcode = require('qrcode-terminal');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,21 +11,30 @@ const client = new Client({
     authStrategy: new LocalAuth(),
 });
 
+const N8N_WEBHOOK_URL = 'https://automation.kuut.us/webhook-test/2a4b0138-8604-4f77-8e1a-be2cdd548abd'; // Set your n8n webhook URL here
+
 client.on('message', async message => {
     console.log(`Received message: ${message.body}`);
 
-    // Trigger your N8 webhook
-    await axios.post('https://automation.kuut.us/webhook-test/2a4b0138-8604-4f77-8e1a-be2cdd548abd', {
-        message: message.body,
-        from: message.from
-    });
+    // Trigger your n8n webhook
+    try {
+        await axios.post(N8N_WEBHOOK_URL, {
+            message: message.body,
+            from: message.from
+        });
+    } catch (error) {
+        console.error('Error sending data to n8n webhook:', error);
+    }
 
     // Send a reply
     message.reply('Message received!');
 });
 
 client.on('qr', qr => {
-    console.log('QR RECEIVED', qr);
+    console.log('QR Code received');
+    qrcode.generate(qr, { small: true }, code => {
+        console.log(code);
+    });
 });
 
 client.on('ready', () => {
